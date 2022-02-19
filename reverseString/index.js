0800fe4d85930eaf4f27693f4d6d23bb87c0c7fa
sash9696 <67851308+sash9696@users.noreply.github.com>
2022-02-19 21:56:19 +0530

    Add files via upload

---
+++ reverseString/index.js
@@ -0,0 +1,62 @@
+// Directions
+// Return a string with the order of characters reversed
+// --- Examples
+//   reverse('abcd') === 'dcba'
+//   reverse('Hello!') === '!olleH'
+
+function reverse(str) {
+	// let arr = []
+	// let new_arr = []
+	// arr = Array.from(str) //str.split('')
+	// console.log(arr)
+	// for(let i=arr.length - 1; i>= 0; i--){
+    //     new_arr.push(arr[i])
+	// }
+	
+	// return new_arr.join('')
+	// let res = ''
+	// for(let i = 0; i< str.length; i++){
+	// 	res = str[i] + res
+	// }
+	// return res
+	return str.split('').reduce((acc, curr)=>{
+		acc = curr + acc
+		return acc
+	},"")
+}
+reverse("abcde")
+
+// _________ _______  _______ _________   _______  _______  _______  _______  _______
+// \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
+//    ) (   | (    \/| (    \/   ) (     | (    \/| (   ) || (    \/| (    \/| (    \/
+//    | |   | (__    | (_____    | |     | |      | (___) || (_____ | (__    | (_____
+//    | |   |  __)   (_____  )   | |     | |      |  ___  |(_____  )|  __)   (_____  )
+//    | |   | (            ) |   | |     | |      | (   ) |      ) || (            ) |
+//    | |   | (____/\/\____) |   | |     | (____/\| )   ( |/\____) || (____/\/\____) |
+//    )_(   (_______/\_______)   )_(     (_______/|/     \|\_______)(_______/\_______)
+//                             ____       _
+//                             |  _ \     | |
+//                             | |_) | ___| | _____      __
+//                             |  _ < / _ \ |/ _ \ \ /\ / /
+//                             | |_) |  __/ | (_) \ V  V /
+//                             |____/ \___|_|\___/ \_/\_/
+//                         ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+//                          ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+//                          ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+mocha.setup('bdd');
+const { assert } = chai;
+
+describe('String Reversal', () => {
+	it('reverse() correctly reverses string', () => {
+		assert.equal(reverse('ffaa'), 'aaff');
+		assert.equal(reverse('  aaff'), 'ffaa  ');
+	});
+});
+
+mocha.run();
